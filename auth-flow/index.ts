46301b77eabf2e79f5b1d59415f21e28ee18b202
Pedro Henrique Soares Cardoso <pedrohenriquesoarescardoso@MacBook-Air-de-Pedro.local>
2024-07-22 17:12:48 -0300

    add nonce

--- auth-flow/index.ts
+++ auth-flow/index.ts
@@ -1,13 +1,30 @@
+import { randomBytes } from 'crypto';
 import express from 'express';
+import { jwtDecode } from 'jwt-decode';
+import session from 'express-session';
 
 const app = express();
 
+app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+}));  
+
 app.get('/login', (req, res) => {
+
+    const nonce: string = randomBytes(16).toString("base64");
+    
+    //@ts-expect-error
+    req.session.nonce = nonce;
+    req.session.save()
+
     const loginParams = new URLSearchParams({
         client_id: 'fdq-client',
         redirect_uri: 'http://localhost:3000/callback',
         response_type: 'code',
-        scope: 'openid'
+        scope: 'openid',
+        nonce
     });
 
     const url = `http://localhost:8080/realms/fdq-realm/protocol/openid-connect/auth?${loginParams.toString()}`
@@ -17,14 +34,14 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/callback', async (req, res) => {
+
     console.log('query', req.query)
 
     const bodyParams = new URLSearchParams({
         client_id: "fdq-client",
         grant_type: "authorization_code",
         code: req.query.code?.toString() as string,
-        redirect_uri: "http://localhost:3000/callback",
-        
+        redirect_uri: "http://localhost:3000/callback",        
     })
     
     const url = "http://host.docker.internal:8080/realms/fdq-realm/protocol/openid-connect/token"
@@ -40,6 +57,15 @@ app.get('/callback', async (req, res) => {
     const result = await response.json();
 
     console.log('response: ', result);
+
+    const payloadIdTokenNonce = jwtDecode(result.id_token as string);
+
+    //@ts-expect-error
+    const requestNonce = req.session.nonce;
+
+    if(payloadIdTokenNonce.nonce !== requestNonce ) {
+       return res.status(401).json({message: 'Unauthenticadet'});
+    }
 
     res.json(result)
 })
